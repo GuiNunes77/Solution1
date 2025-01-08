45caec906df810c080cf141fafbe09f65f282a47
Guilherme Nunes <nunes.77@hotmail.com>
2025-01-07 23:20:17 -0300

    more token info

---
+++ ClientLibrary/Helpers/CustomAuthenticationStateprovider.cs
@@ -0,0 +1,25 @@
+﻿using BaseLibrary.DTOs;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace ClientLibrary.Helpers
+{
+    public class CustomAuthenticationStateProvider(LocalStorageService localStorageService) : AuthenticationStateProvider
+    {
+        private readonly ClaimsPrincipal anonymous = new (new ClaimsIdentity());
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var stringToken = await localStorageService.GetToken();
+            if (string.IsNullOrEmpty(stringToken)) return await Task.FromResult(new AuthenticationState(anonymous));
+
+            var deserializedToken = Serializations.DeserializeJsonString<UserSession>(stringToken);
+            if (deserializedToken == null) return await Task.FromResult(new AuthenticationState(anonymous));
+
+            var getUserClaims = DecryptToken(deserializedToken.Token!);
+            if (getUserClaims == null) return await Task.FromResult(new AuthenticationState(anonymous));
+
+            var claimsPrincipal = SetClaimPrincipal(getUserClaims);
+            return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+        }
+    }
+}
